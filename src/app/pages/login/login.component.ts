ef2f408f78d2dc82b00febcd6fb21de85e6ffd48
Ramiro Guzmán C. <ramiroguzmanc@gmail.com>
2024-08-13 10:43:08 -0500

    Maquetación del login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+
+}
